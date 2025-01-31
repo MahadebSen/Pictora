6ef8601bd2b75d615f74193da2464b824f1e9b80
Mahadeb Sen <mahadebsen237@gmail.com>
2025-01-31 23:52:01 +0530

    Added nav links in mobile nav component.

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -2,7 +2,19 @@
 import { navLinks } from "@/constants";
 
 const MobileNav = () => {
-  return <div>MobileNav</div>;
+  return (
+    <div>
+      {navLinks.map((link) => (
+        <a
+          href={link.route}
+          key={link.route}
+          className="block py-2 text-center text-gray-600 hover:text-gray-800"
+        >
+          {link.label}
+        </a>
+      ))}
+    </div>
+  );
 };
 
 export default MobileNav;
